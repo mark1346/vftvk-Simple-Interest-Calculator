37cf69d66b0c87182debe2010578a24093419fc6
Mark Han <markhan@eduroam-int-dhcp-97-37-177.ucl.ac.uk>
2021-09-30 17:10:38 +0100

    submit

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    validateForm();
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var resultAmount = parseInt(principal) + parseInt(interest);
+    var result = "If you deposit " + principal + ", \<br\>at an interest rate of " + rate + "%. You will receive an amount of " + 
+                resultAmount + ", in the year " + year + "\<br\>";
+    document.getElementById("result").innerText = result;
 }
-        
+
+function validateForm()
+{
+    var principal = document.getElementById("principal").value;
+    if(parseInt(principal) <= 0){
+        alert("Enter a positive number");
+        return false;
+    }
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
